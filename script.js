6f17cb29d5901e63de33dc8f22b4898897a5023a
ShafiArif8 <shafiengineer8@gmail.com>
2025-02-09 00:40:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+var sec1 = document.getElementById('sec1');
+var sec2 = document.getElementById('sec2');
+var sec3 = document.getElementById('sec3');
+var sec4 = document.getElementById('sec4');
+function name1(){
+    sec1.style.display = "none";
+    sec2.style.display = "block";
+    sec3.style.display = "none";
+    sec4.style.display = "none";
+}
+function name2(){
+    sec1.style.display = "none";
+    sec2.style.display = "none";
+    sec3.style.display = "block";
+    sec4.style.display = "none";
+}
+function name3(){
+    sec1.style.display = "none";
+    sec2.style.display = "none";
+    sec3.style.display = "none";
+    sec4.style.display = "block";
+}
